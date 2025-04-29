34c710c6c253d059825269ab6858fe172958aad7
김태은 <iostream1701@gmail.com>
2025-04-29 17:19:41 +0900

    버셀 배포

--- server/server.ts
+++ server/server.ts
@@ -26,14 +26,5 @@ const db = new MongoDB(process.env.MONGODB_URI || '', 'dalmuti');
 const gameManager = new GameManager(db);
 new SocketManager(io, gameManager);
 
-const PORT = process.env.PORT || 3000;
-
-httpServer.listen(
-  {
-    port: PORT,
-    host: '0.0.0.0',
-  },
-  () => {
-    console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
-  }
-);
+// Vercel에서는 이 부분을 제거하고 app을 export
+export default app;
